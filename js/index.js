9ea9f504b932ed3de2d07ef3f73dbf1d57e04693
Ambuj <thecoderguy@outlook.com>
2018-03-11 10:17:53 +0530

    y

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 function searchFunction(){
   var input, filter, ul, li, a, i;
-  input=document.getElementById('searchbar');
+  input=document.getElementById('ainput');
   filter=input.value.toUpperCase();
   ul= document.getElementById('wrapper');
-  li= ul.getElementByTagName('li');
+  li= ul.getElementByTagName('div');
 
   for(i=0;i<li.length;i++)
   {
-    a= li[i].getElementByTagName('a')[0];
+    a= li[i].getElementByTagName('h5')[0];
     if(a.innerHTML.toUpperCase().indexOf(filter)> -1){
       li[i].style.display= "";
     }

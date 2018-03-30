bae0416341898e2f284ab505c28806af4e4d4562
Ambuj <thecoderguy@outlook.com>
2018-03-31 00:11:14 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,3 +19,11 @@ function searchFunction(){
 function themeChange() {
   document.body.style.backgroundColor = #111111;
 }
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
